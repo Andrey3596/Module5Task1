a46b4f31b3ae756a60efaf3876974c5271bb066f
Limolix <andrejnagirnak395@gmail.com>
2026-03-22 18:12:23 +0800

    DrawRectangle

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,15 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.Clear(Color.White);
+
+            g.FillRectangle(new SolidBrush(Color.Yellow), 200, 100, 50, 30);
+            g.DrawRectangle(new Pen(Color.Red, 2), 200, 100, 50, 30);
+        }
     }
 }
